40c2bef2a51f6e9af16f363c4f24133f8bbbf9bf
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-02-13 02:17:28 +0600

    first added this section is triangle

---
+++ js/parallelogram.js
@@ -0,0 +1,45 @@
+
+// function calculateParallelogramArea(){
+    // get parallelogram base value
+//     const parallelogramBaseInput = document.getElementById('parallelogram-base');
+//     const parallelogramBaseText = parallelogramBaseInput.value;
+//     const base = parseFloat(parallelogramBaseText);
+//     console.log(base);
+
+    // get parallelogram height value
+//     const parallelogramHeightInput = document.getElementById('parallelogram-height');
+//     const parallelogramHeightText = parallelogramHeightInput.value;
+//     const height = parseFloat(parallelogramHeightText);
+//     console.log(height);
+
+    // calculate parallelogram area
+//     const area = base * height;
+//     console.log('area of the parallelogram is:', area);
+
+    // display parallelogram area
+//     const parallelogramAreaSpan = document.getElementById('parallelogram-area');
+//     parallelogramAreaSpan.innerText = area;
+// }
+
+// function calculateParallelogramArea(){
+//     const base = getElementById('parallelogram-base');
+
+//     const height = getElementById('parallelogram-height');
+
+//     const area = base * height;
+//     console.log('area of the parallelogram is:', area);
+
+//     setInnerTextId('parallelogram-area', area);
+// }
+
+// function getElementById(inputFieldId){
+//     const inputField = document.getElementById(inputFieldId);
+//     const inputValueText = inputField.value;
+//     const inputValue = parseFloat(inputValueText);
+//     return inputValue;
+// }
+
+// function setInnerTextId(elementId, area){
+//     const element = document.getElementById(elementId);
+//     element.innerText = area;
+// }
---
+++ js/rectangle.js
@@ -0,0 +1,23 @@
+// console.log('rect connected');
+
+// function calculateRectangleArea(){
+//     // get rectangle width value
+//     const rectangleWidthInput = document.getElementById('rectangle-width');
+//     const rectangleWidthText = rectangleWidthInput.value;
+//     const width = parseFloat(rectangleWidthText);
+//     console.log(width);
+
+//     // get rectangle length value
+//     const rectangleLengthInput = document.getElementById('rectangle-length');
+//     const rectangleLengthText = rectangleLengthInput.value;
+//     const length = parseFloat(rectangleLengthText);
+//     console.log(length);
+
+//     // calculate rectangle area
+//     const area = width * length;
+//     console.log('area of the rectangle is:', area);
+
+//     // display rectangle area
+//     const rectangleAreaSpan = document.getElementById('rectangle-area');
+//     rectangleAreaSpan.innerText = area;
+// }
